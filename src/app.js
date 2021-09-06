03a838059c71c3c65127da55c1d83390bf55a01a
Rahul Pillai <rahul.pillai93@gmail.com>
2021-09-06 16:14:32 -0700

    Fixed adding 404 pages

--- src/app.js
+++ src/app.js
@@ -34,68 +34,68 @@ app.get('/about', (req, res) => {
     })
 })
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        helpText: 'This is some helpful text.',
-        title: 'Help',
-        name: 'Rahul Pillai'
-    })
-})
+//app.get('/help', (req, res) => {
+//    res.render('help', {
+//        helpText: 'This is some helpful text.',
+//        title: 'Help',
+//        name: 'Rahul Pillai'
+//    })
+//})
 
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
-    }
+//app.get('/weather', (req, res) => {
+//    if (!req.query.address) {
+//        return res.send({
+//            error: 'You must provide an address!'
+//        })
+//    }
 
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-        if (error) {
-            return res.send({ error })
-        }
+//    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+//        if (error) {
+//            return res.send({ error })
+//        }
 
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({ error })
-            }
+//        forecast(latitude, longitude, (error, forecastData) => {
+//            if (error) {
+//                return res.send({ error })
+//            }
 
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
-        })
-    })
-})
+//            res.send({
+//                forecast: forecastData,
+//                location,
+//                address: req.query.address
+//            })
+//        })
+//    })
+//})
 
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
-    }
+//app.get('/products', (req, res) => {
+//    if (!req.query.search) {
+//        return res.send({
+//            error: 'You must provide a search term'
+//        })
+//    }
 
-    console.log(req.query.search)
-    res.send({
-        products: []
-    })
-})
+//    console.log(req.query.search)
+//    res.send({
+//        products: []
+//    })
+//})
 
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Rahul Pillai',
-        errorMessage: 'Help article not found.'
-    })
-})
+//app.get('/help/*', (req, res) => {
+//    res.render('404', {
+//        title: '404',
+//        name: 'Rahul Pillai',
+//        errorMessage: 'Help article not found.'
+//    })
+//})
 
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Rahul Pillai',
-        errorMessage: 'Page not found.'
-    })
-})
+//app.get('*', (req, res) => {
+//    res.render('404', {
+//        title: '404',
+//        name: 'Rahul Pillai',
+//        errorMessage: 'Page not found.'
+//    })
+//})
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
